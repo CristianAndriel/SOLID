d75c016b1660882c3cb8491efaa7f1297aa647cf
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-14 14:38:59 -0300

    Criando o repository do carro

--- src/S/Repository/CarRepository.java
+++ src/S/Repository/CarRepository.java
@@ -1,4 +1,9 @@
 package S.Repository;
+import S.Models.Car;
 
 public class CarRepository {
+    public void save(Car car) {
+        String model = car.getModel();
+        System.out.println("Salvando carro" + model + " no banco de dados");
+    }
 }
