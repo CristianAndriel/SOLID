80b153e018b64ca5cad6822a5d8145cba0389040
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-14 14:31:30 -0300

    main - exemplo de uso

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import S.User;
+import S.Service.EmailService;
+import S.Repository.UserRepository;
+
 public class Main {
     public static void main(String[] args) {
+        // 1. Criar o usuário
+        User user = new User("João Silva", "joao@email.com");
 
+        // 2. Salvar no banco de dados
+        UserRepository repository = new UserRepository();
+        repository.save(user);
+
+        // 3. Enviar email de boas-vindas
+        EmailService emailService = new EmailService();
+        emailService.sendWelcomeEmail(user);
     }
 }
